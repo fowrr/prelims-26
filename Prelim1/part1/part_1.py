b2854e98930f85a83ee3bba40d01554cd2e40f38
fowrr <smand3539@gmail.com>
2025-11-03 17:10:11 -0500

    initial commit
    i got this >:)

---
+++ Prelim1/part1/part_1.py
@@ -0,0 +1,39 @@
+# For your imports look at the rulebook to see what is allowed
+# Pour les import permis allez vous référer au livre des règlements
+"""part_1.py
+This is the template file for the part 1 of the Prelim 1.
+Ceci est le fichier template pour la partie 1 du Prelim 1.
+"""
+
+# just a small helper to visualize the tail
+def print_tail(tail: [str]):
+    for line in tail:
+        print(line)
+
+def part_1(size: int):
+    """
+    Draw the platypus tail
+
+    Parameters:
+        size int: Size of the tail
+
+    Returns:
+        [String]: The platypus tail drawn
+    """
+    tail = []
+    ### YOUR CODE GOES HERE ###
+    print(size)
+    char_list = ["|","_", "/", " ", "\\"] 
+    tailB_h = size + 1
+    tailB_l = size * 2
+    tail_body = []
+    for x in range (0, tailB_h):
+        tail_bodyLine = []
+        tail_bodyLine += char_list[0]
+        for y in range (0, tailB_l):
+
+    print(tail_body)
+    #test edit
+
+    return tail
+part_1(2)
---
+++ Prelim1/part5/part_5.py
@@ -0,0 +1,26 @@
+# For your imports look at the rulebook to see what is allowed
+# Pour les import permis allez vous référer au livre des règlements
+"""part_5.py
+This is the template file for the part 5 of the Prelim 1.
+Ceci est le fichier template pour la partie 5 du Prelim 1.
+"""
+
+def part_5(turns: int, board: [str]):
+    """
+    Simulate the Game of platypus
+
+    Parameters:
+        turns str: The number of turns in the game
+        board [str]: The 16x16 grid representing the board of the Game of platypus with x as the platypus and . the food
+
+    Returns:
+        str: Is the platypus surviving ("Yes" or "No")
+    """
+    final_answer = "No"
+    ### You code goes here ###
+    ### Votre code va ici ###
+
+
+
+
+    return final_answer
